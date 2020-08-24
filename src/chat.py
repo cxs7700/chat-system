c369f0b1be221de1e06d123ef14c6b72d4a28547
Christopher Savan <cxs7700@rit.edu>
2020-08-24 18:38:38 -0400

    test rebuilds tables is idempotent and tag

---
+++ src/chat.py
@@ -0,0 +1,29 @@
+from src.swen344_db_utils import connect
+
+def buildTables():
+  conn = connect()
+  cur = conn.cursor()
+  sql = """
+      CREATE TABLE example_table(
+          example_col VARCHAR(40)
+      )
+  """
+  cur.execute(sql)
+  conn.commit() # don't forget this!
+  conn.close()
+  
+def rebuildTables():
+    conn = connect()
+    cur = conn.cursor()
+    drop_sql = """
+        DROP TABLE IF EXISTS example_table
+    """
+    create_sql = """
+        CREATE TABLE example_table(
+            example_col VARCHAR(40)
+        )
+    """
+    cur.execute(drop_sql)
+    cur.execute(create_sql)
+    conn.commit()
+    conn.close()
---
+++ tests/test_chat.py
@@ -0,0 +1,24 @@
+import unittest
+from src.chat import *
+from src.swen344_db_utils import connect
+
+class TestChat(unittest.TestCase):
+
+    def test_build_tables(self):
+        """Build the tables"""
+        conn = connect()
+        cur = conn.cursor()
+        rebuildTables()
+        cur.execute('SELECT * FROM example_table')
+        self.assertEqual([], cur.fetchall(), "no rows in example_table")
+        conn.close()
+      
+    def test_rebuild_tables_is_idempotent(self):
+      """Drop and rebuild the tables twice"""
+      rebuildTables()
+      rebuildTables()
+      conn = connect()
+      cur = conn.cursor()
+      cur.execute('SELECT * FROM example_table')
+      self.assertEqual([], cur.fetchall(), "no rows in example_table")
+      conn.close()
